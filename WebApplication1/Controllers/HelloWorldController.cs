2d99b2c9495cb58867e9df8f75f6eb656697b56b
KuaqSon <qson.ng@gmail.com>
2018-06-14 16:24:39 +0700

    Add project files.

---
+++ WebApplication1/Controllers/HelloWorldController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Welcome(string name, int numtimes  = 1)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.Numtimes = numtimes;
+            return View();
+        }
+
+
+        //public string Index()
+        //{
+        //    return "This is my <b>default</b> action...";
+        //}
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        //public string Welcome(string name, int numTimes = 1)
+        //{
+        //    return HttpUtility.HtmlEncode("Hello " + name + ", NumTimes is: " + numTimes);
+        //}
+
+
+    }
+}
