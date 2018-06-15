5481270b125703905f2981f933181c84319277b2
KuaqSon <qson.ng@gmail.com>
2018-06-15 09:58:31 +0700

    add customer (view list & create new)

---
+++ WebApplication1/Controllers/CustomersController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+
+namespace WebApplication1.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private CustomerDBContext db = new CustomerDBContext();
+
+        //GET: Customer 
+        public ActionResult Index()
+        {
+            return View(db.Customers.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult Create(string Title)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Customers.Add(customer);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(customer);
+        }
+
+
+
+
+    }
+}
--- WebApplication1/Models/Customer.cs
+++ WebApplication1/Models/Customer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
@@ -7,10 +9,20 @@ namespace WebApplication1.Models
 {
     public class Customer
     {
+        public int ID { get; set; }
         public string Name { get; set; }
-        public int Age { get; set; }
+
+        [Display(Name = "Date Of Birth")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime Dob { get; set; }
+        public string Gender { get; set; }
         public string Address { get; set; }
         public string PhoneNumber { get; set; }
+    }
 
+    public class CustomerDBContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
     }
 }
